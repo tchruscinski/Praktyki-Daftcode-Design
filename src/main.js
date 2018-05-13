517224c1f1afb67229eddc54884bc209f17948b4
Tomasz Chruściński <tom.chruscinski@gmail.com>
2018-05-14 00:31:12 +0200

    The animation works

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ let options = document.querySelectorAll('.option'),
     object  = document.querySelector('svg');
 
 object.addEventListener('click', function(){
-    // object.classList.add("active");
-    console.log('hello');
-    // setTimeout(function(){
-    // object.classList.remove("active");
-    // }, 2000);
+    object.classList.add("active");
+    setTimeout(function(){
+    object.classList.remove("active");
+    }, 2000);
 });
 
 options.forEach(function(option){
